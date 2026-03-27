88b582e6e43383fb77c4da188d34d293c648c143
Hayrapetyan Zhirayr <zhsayrapetyan@yandex.ru>
2026-03-27 23:01:00 +0000

    feat: add consolidate_ckpt.sh script for checkpoint consolidation

---
+++ scripts/consolidate_ckpt.sh
@@ -0,0 +1,13 @@
+VENV_DIR="${1:?VENV_DIR is required}"
+AUTOMODEL_DIR="${2:?AUTOMODEL_DIR is required}"
+BASE_MODEL="${3:?BASE_MODEL is required}"
+CKPT_SHARDED="${4:?CKPT_SHARDED is required}"
+CKPT_CONSOLIDATED="${5:?CKPT_CONSOLIDATED is required}"
+BACKEND="${6:-gloo}"
+source "$VENV_DIR/bin/activate"
+
+"$VENV_DIR/bin/python" "$AUTOMODEL_DIR/tools/offline_hf_consolidation.py" \
+    -m $BASE_MODEL \
+    -i $CKPT_SHARDED \
+    -o $CKPT_CONSOLIDATED \
+    --backend $BACKEND \
