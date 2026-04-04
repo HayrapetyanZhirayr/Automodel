6e463c8be42b784c90461cf5a975c36e8513b83b
deniskokosss <deniskokosss@mail.ru>
2026-04-04 12:35:00 +0000

    chore: add num_threads parameter to consolidate_ckpt.sh

--- scripts/consolidate_ckpt.sh
+++ scripts/consolidate_ckpt.sh
@@ -4,6 +4,7 @@ BASE_MODEL="${3:?BASE_MODEL is required}"
 CKPT_SHARDED="${4:?CKPT_SHARDED is required}"
 CKPT_CONSOLIDATED="${5:?CKPT_CONSOLIDATED is required}"
 BACKEND="${6:-gloo}"
+NUM_THREADS="${7:-16}"
 source "$VENV_DIR/bin/activate"
 
 "$VENV_DIR/bin/python" "$AUTOMODEL_DIR/tools/offline_hf_consolidation.py" \
@@ -11,3 +12,4 @@ source "$VENV_DIR/bin/activate"
     -i $CKPT_SHARDED \
     -o $CKPT_CONSOLIDATED \
     --backend $BACKEND \
+    --num-threads $NUM_THREADS \
